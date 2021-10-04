cfa222961ada77d0bbecc3ddd7d2f15e9ab95384
Ho Nguyen <ho.nguyen@wizeline.com>
2021-10-04 17:29:50 +0700

    Correct to generate extent report while running with command

--- src/test/java/ecommerce_test/listeners/ExtentReporterNG.java
+++ src/test/java/ecommerce_test/listeners/ExtentReporterNG.java
@@ -1,6 +1,5 @@
 package ecommerce_test.listeners;
 
-import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -14,54 +13,60 @@ import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.xml.XmlSuite;
 
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 
 public class ExtentReporterNG implements IReporter {
-    protected ExtentReports extent;
+    private ExtentReports extent;
+    ExtentHtmlReporter htmlReporter;
 
     public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
-        extent = new ExtentReports(outputDirectory + File.separator
-                + "Extent.html", true);
 
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir")+"/reports/report.html");
+        extent = new ExtentReports();
+
+        extent.attachReporter(htmlReporter);
         for (ISuite suite : suites) {
             Map<String, ISuiteResult> result = suite.getResults();
 
             for (ISuiteResult r : result.values()) {
                 ITestContext context = r.getTestContext();
 
-                buildTestNodes(context.getPassedTests(), LogStatus.PASS);
-                buildTestNodes(context.getFailedTests(), LogStatus.FAIL);
-                buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
+                buildTestNodes(context.getPassedTests(), Status.PASS);
+                buildTestNodes(context.getFailedTests(), Status.FAIL);
+                buildTestNodes(context.getSkippedTests(), Status.SKIP);
             }
         }
 
         extent.flush();
-        extent.close();
+
     }
 
-    private void buildTestNodes(IResultMap tests, LogStatus status) {
+    private void buildTestNodes(IResultMap tests, Status status) {
         ExtentTest test;
 
         if (tests.size() > 0) {
             for (ITestResult result : tests.getAllResults()) {
-                test = extent.startTest(result.getMethod().getMethodName());
+                test = extent.createTest(result.getMethod().getMethodName());
 
-                test.setStartedTime(getTime(result.getStartMillis()));
-                test.setEndedTime(getTime(result.getEndMillis()));
+                /*test.getTest(). = getTime(result.getStartMillis());
+                test.getTest().endedTime = getTime(result.getEndMillis());*/
 
                 for (String group : result.getMethod().getGroups())
                     test.assignCategory(group);
 
-                if (result.getThrowable() != null) {
-                    test.log(status, result.getThrowable());
-                } else {
-                    test.log(status, "Test " + status.toString().toLowerCase()
-                            + "ed");
-                }
+                String message = "Test " + status.toString().toLowerCase() + "ed";
 
-                extent.endTest(test);
+                if (result.getThrowable() != null)
+                    message = result.getThrowable().getMessage();
+
+                test.log(status, message);
+
+                //      extent.endTest(test);
             }
         }
     }
